4f7432f31e7725e65b325468fe1c57af9f98f703
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2023-07-24 15:22:00 +1000

    request for manage external storage

--- app/src/main/java/jatx/mybooks/MainActivity.kt
+++ app/src/main/java/jatx/mybooks/MainActivity.kt
@@ -1,15 +1,20 @@
 package jatx.mybooks
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import jatx.mybooks.util.Backup.loadLauncher
 import jatx.mybooks.util.Backup.saveLauncher
 import jatx.mybooks.util.Backup.tryToLoadBackup
 import jatx.mybooks.util.onLoadPermissionGranted
 import jatx.mybooks.util.onSavePermissionGranted
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,6 +40,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestForManageExternalStorage()
+
         initBookList()
     }
 
@@ -44,6 +51,18 @@ class MainActivity : AppCompatActivity() {
             tryToLoadBackup()
         } else {
             onLoadPermissionGranted()
+        }
+    }
+
+    private fun requestForManageExternalStorage() {
+        if (Build.VERSION.SDK_INT >= 30) {
+            if (!Environment.isExternalStorageManager()) {
+                val intent = Intent()
+                intent.action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+                val uri = Uri.fromParts("package", this.packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
         }
     }
 }
